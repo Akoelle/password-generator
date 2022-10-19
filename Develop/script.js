aaa7c9f491cacb8c1550e09f24dd6b6ebf014727
Akoelle <annakoelle1@gmail.com>
2022-10-19 06:14:07 -0700

    started homework 3

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,35 @@
 // Assignment code here
 
+//select button in html
+var createPasswordButton=document.querySelector("#generate")
+console.log (createPasswordButton)
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+//this is what happens when you click the button
+function createPassword(){
+  //ask user what length of password should be
+  var passwordlength=prompt("how long is your password")
+  console.log(passwordlength)
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// make button do something when you click it
+createPasswordButton.addEventListener("click",createPassword)
+
+
+
+
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
